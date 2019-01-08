46fd1ec0e20299201f09ba5bbbcb451c40c221f3
halfwhole <limziwei2@gmail.com>
2019-01-09 00:33:20 +0800

    Add React views

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
